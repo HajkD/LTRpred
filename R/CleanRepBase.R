3030f964e29e89b0c36ffbf54a96b5b1572f76ff
HajkD <hajkdrost@googlemail.com>
2016-01-14 15:55:32 +0000

    add function CleanRepBase()

---
+++ R/CleanRepBase.R
@@ -0,0 +1,29 @@
+#' @title Clean the initial Repbase database for BLAST
+#' @description This function cleans the headers of the Repbase fasta files
+#' so that they can be used to create a blast-able database.
+#' @param repbase.file fasta file storing the corresponding Repbase annotation, e.g. \code{athrep.ref}.
+#' @param output.file name/path of the cleaned Repbase annotation file. 
+#' @author Hajk-Georg Drost
+#' @details The Repbase database can be downloaded after registration at http://www.girinst.org/repbase/.
+#' The corresponding files as they are however, cannot be converted into a blast-able database.
+#' Hence, a pre-filtering step is neccessary to be able to use this database with the e.g. \code{\link{QueryRepBase}}
+#' function.  
+#' @examples 
+#' \dontrun{
+#' # PreProcess Repbase: A thaliana
+#' # and save the output into the file "Athaliana_repbase.ref"
+#' CleanRepBase(repbase.file = "athrep.ref",
+#'              output.file  = "Athaliana_repbase.ref")
+#' 
+#' }
+#' @references http://www.girinst.org/repbase/
+#' @export
+CleanRepBase <- function(repbase.file, output.file){
+    
+    repbase <- Biostrings::readDNAStringSet(repbase.file)
+    repbase@ranges@NAMES <- stringr::str_replace_all(repbase@ranges@NAMES," ","_")
+    repbase@ranges@NAMES <- unlist(lapply(stringr::str_split(repbase@ranges@NAMES, "\t"), paste0, collapse = "_"))
+    Biostrings::writeXStringSet(repbase,output.file)
+    
+}
+
