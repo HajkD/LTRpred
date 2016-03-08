b66f114c5c6410a6a08c354c683fcc51cec12f0c
HajkD <hajkdrost@googlemail.com>
2016-03-08 16:11:50 +0000

    adding correlation analysis to PlotSizeCorrelation()

--- R/PlotSizeCorrelation.R
+++ R/PlotSizeCorrelation.R
@@ -1,10 +1,12 @@
 #' @title Plot Genome size vs. LTR transposon count
 #' @description Genome size vs. LTR transposon count.
 #' @param genome.matrix Genome matrix retruned by \code{\link{LTRpred.meta}}.
+#' @param cor.method correlation analysis method. Either \code{"pearson"}, \code{"kendall"}, or \code{"spearman"}. 
 #' @param xlab x-axis label.
 #' @param ylab y-axis label.
 #' @param main main text.
 #' @param text.size size of the labels in ggplot2 notation.
+#' @param check.overlap shall overlaps of labels be avoided or not.
 #' @author Hajk-Georg Drost
 #' @examples 
 #' \dontrun{
@@ -12,24 +14,29 @@
 #' }
 #' @export
 PlotSizeCorrelation <- function(genome.matrix,
-                                xlab      = "LTR transposon Count",
-                                ylim      = "Genome size in bp", 
-                                main      = "Genome size vs. LTR transp. count",
-                                text.size = 18){
+                                cor.method    = "spearman",
+                                xlab          = "LTR transposon Count",
+                                ylim          = "Genome size in bp", 
+                                main          = "Genome size vs. LTR transp. count",
+                                text.size     = 18,
+                                check.overlap = TRUE){
   
+  cor.value <- cor(genome.matrix$nLTRs,genome.matrix$genome.size, method = cor.method)
+
   colnames(genome.matrix)[1] <- "organism"
   res <- ggplot2::ggplot(genome.matrix, ggplot2::aes(x = nLTRs, y = genome.size)) + ggplot2::geom_point() + 
          ggplot2::theme_minimal() + 
-         ggplot2::labs(x = xlab, y = ylim, title = main) + 
+         ggplot2::labs(x = xlab, y = ylim, title = paste0(main, " [ Corr ( ",cor.method," ) = ",round(cor.value, digits = 2), " ]")) + 
          ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) + 
          ggplot2::theme(axis.title  = ggplot2::element_text(size = text.size,face = "bold"),axis.text.y = ggplot2::element_text(size = text.size,face = "bold"),
                         axis.text.x = ggplot2::element_text(size = text.size,face = "bold"),
                         panel.background = ggplot2::element_blank(), 
-                        plot.title = ggplot2::element_text(size = text.size, colour = "black", face = "bold"))
+                        plot.title = ggplot2::element_text(size = text.size, colour = "black", face = "bold")) +
+    ggplot2::geom_text(ggplot2::aes(label = organism),hjust=0, vjust=0,size = 3, check_overlap = check.overlap)
   
   return (res)
 }
 
-#          ggplot2::geom_text(ggplot2::aes(label = organism),hjust=0, vjust=0,size = 6) +
+#          ggplot2::geom_text(ggplot2::aes(label = organism),hjust=0, vjust=0,size = 6, check_overlap = TRUE) +
 
 
