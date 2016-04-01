31486b69d59e469a56024f7ef54e57834c85b8d1
HajkD <hajkdrost@googlemail.com>
2016-04-01 16:33:35 +0200

    update file.move()

--- R/file.move.R
+++ R/file.move.R
@@ -1,4 +1,4 @@
-# taken from: http://stackoverflow.com/questions/10266963/moving-files-between-folders
+# adapted from: http://stackoverflow.com/questions/10266963/moving-files-between-folders
 file.move <- function(from, to) {
   todir <- dirname(to)
   if (!isTRUE(file.info(todir)$isdir)) dir.create(todir, recursive=TRUE)
