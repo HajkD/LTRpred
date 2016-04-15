94b4a9b48cd35200a1f67b0e74a2a5888260f3f8
HajkD <hajkdrost@googlemail.com>
2016-04-15 13:50:31 +0100

    adding documenttion for file.move()

--- R/file.move.R
+++ R/file.move.R
@@ -1,3 +1,11 @@
+#' @title Move folders from one location to another
+#' @description A small helper function to make file handling
+#' in R easier.
+#' @param from path from where to copy a folder.
+#' @param to path where to copy the corresponding folder specified in \code{from}.
+#' @author Hajk-Georg Drost
+#' @export
+
 # adapted from: http://stackoverflow.com/questions/10266963/moving-files-between-folders
 file.move <- function(from, to) {
   todir <- dirname(to)
