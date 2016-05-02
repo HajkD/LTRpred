77e842584046f3bdd044546902ff6a900e5f51dd
HajkD <hajkdrost@googlemail.com>
2016-05-02 15:49:15 +0100

    update pred2annotation file

--- R/pred2annotation.R
+++ R/pred2annotation.R
@@ -2,7 +2,7 @@
 #' @description Match \code{\link{LTRharvest}}, \code{\link{LTRdigest}}, or \code{\link{LTRpred}} predictions with a given annotation file in GFF3 format
 #' to check whether or not predicted LTR transposons overlap with annotated genomic features (in this case with genes).
 #' @param pred.file a \code{data.frame} returned by \code{\link{LTRharvest}}, \code{\link{LTRdigest}}, or \code{\link{LTRpred}}.
-#' @param annotation.file an genome annotation file in gff3 format.
+#' @param annotation.file imported genome annotation file from gff3 file.
 #' @param strand.ori the strand orientation that shall be matched between the the prediction and the annotation.
 #' Please note that some predictions do not include strand information. In case strand information should not be considered
 #' please specify \code{strand.ori = NULL}.
@@ -26,11 +26,16 @@
 #' LTRdigest <- read.prediction(gff.file    = "TAIR10_chr_all_LTRdigestPrediction.gff",
 #'                              tabout.file = "TAIR10_chr_all-ltrdigest_tabout.csv",
 #'                              program     = "LTRdigest")
-#'                                             
+#' # read annotation file
+#' # read the annotation file in gff3 format
+#' AnnotationFileGFF3 <- readr::read_tsv("Arabidopsis_thaliana.TAIR10.30.chr.gff3", col_names = FALSE, skip = 0, comment = "#")
+#' names(AnnotationFileGFF3)[1:9] <- c("chromosome","source","feature","start","end","score","strand","frame","attribute")
+#'                                              
+#'                                        
 #' # Match predicted LTR transposons with annotation file (feature = genes)        
 #' Anno <- pred2annotation(
 #'                pred.file = Ath.LTRdigest.prediction$ltr.retrotransposon,
-#'                annotation.file       = "Arabidopsis_thaliana.TAIR10.30.chr.gff3")
+#'                annotation.file       = AnnotationFileGFF3)
 #' }
 #' @export
 
@@ -42,19 +47,14 @@ pred2annotation <- function(pred.file,
     if (!is.element(strand.ori,c("+","-")))
         stop ("Please enter the correct strand.ori information! Either '+' strand or '-' strand.")
     
-    chromosome <- strand <- feature <- seqname <- NULL
+    chromosome <- strand <- feature <- NULL
   
-    # read the annotation file in gff3 format
-    AnnotationFileGFF3 <- readr::read_tsv(annotation.file, col_names = FALSE, skip = 0, comment = "#")
-    names(AnnotationFileGFF3)[1:9] <- c("chromosome","source","feature","start","end","score","strand","frame","attribute")
-    
     chrms.annotation <- names(table(AnnotationFileGFF3[ , "chromosome"]))
     chrms.pred <- names(table(pred.file[ , "chromosome"]))
     
     if (!identical(chrms.annotation,chrms.pred))
       stop ("Please check chromosome names in pred.file and annotation.file... 
             chromosome names do not seem to match.")
-    
     
     # match LTRdigest prediction output with Annotation File in GFF3 format
     Annotation <- vector("list")
