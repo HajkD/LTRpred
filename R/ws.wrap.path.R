4907215516c94cb727a775242295815f66588ca0
HajkD <hgd23@cam.ac.uk>
2016-05-09 16:44:13 +0100

    adding new helper function ws.wrap.path() to control white space paths

--- R/ws.wrap.path.R
+++ R/ws.wrap.path.R
@@ -1,3 +1,9 @@
+#' @title Wrap whitespace in paths
+#' @description Helper function for wrapping white spaces in folder paths.
+#' @param paths a string or vector of strings containing paths.
+#' @author Hajk-Georg Drost
+#' @export
+
 ws.wrap.path <- function(paths){
   
   paths <- stringr::str_split(paths,.Platform$file.sep)
@@ -5,11 +11,11 @@ ws.wrap.path <- function(paths){
   ws.paths <- unlist(lapply(paths, function(x) {
     
     stringr::str_c(unlist(sapply(x, function(word) {
-      if(stringr::str_detect(word," ")) 
+      if (stringr::str_detect(word," ")) 
         return(stringr::str_replace(word,word,paste0("'",word,"'"))) else 
           return(word)})),collapse = .Platform$file.sep)
     
   }))
  
-  return (ws.paths) 
+  return(ws.paths) 
 }
