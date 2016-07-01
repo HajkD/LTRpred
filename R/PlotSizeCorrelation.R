ae1b7b3b57abecc612566fc4adf13fe23203587a
HajkD <hgd23@cam.ac.uk>
2016-07-01 15:07:47 +0100

    adding smoothing function (correlation analysis) to PlotSizeCorrelation()

--- R/PlotSizeCorrelation.R
+++ R/PlotSizeCorrelation.R
@@ -1,7 +1,10 @@
 #' @title Plot Genome size vs. LTR transposon count
 #' @description Genome size vs. LTR transposon count.
 #' @param genome.matrix Genome matrix retruned by \code{\link{LTRpred.meta}}.
-#' @param cor.method correlation analysis method. Either \code{"pearson"}, \code{"kendall"}, or \code{"spearman"}. 
+#' @param cor.method correlation analysis method. Either \code{"pearson"}, \code{"kendall"}, or \code{"spearman"}.
+#' @param smooth.method method to add a smoothed conditional mean (see \code{ggplot2::geom_smooth()}).
+#' Users can choose from \code{auto}, \code{glm}, \code{lm}, or \code{loess}. If no smoothing function shall be applied,
+#' users can specify \code{smooth.method = 'NULL'}. 
 #' @param xlab x-axis label.
 #' @param ylab y-axis label.
 #' @param main main text.
@@ -26,6 +29,7 @@
 
 PlotSizeCorrelation <- function(genome.matrix,
                                 cor.method          = "spearman",
+                                smooth.method       = "glm",
                                 xlab                = "LTR transposon Count",
                                 ylab                = "Genome size in Mega [bp]", 
                                 main                = "Genome size vs. LTR transp. count",
@@ -41,80 +45,209 @@ PlotSizeCorrelation <- function(genome.matrix,
                                 check.overlap       = TRUE){
   
   
-  nLTRs <- genome.size <- cl.colors <- organism <- NULL
-  
-  if (cl.analysis){
+    nLTRs <- genome.size <- cl.colors <- organism <- NULL
     
-    if (is.null(sim.matrix))
-      stop ("Please specify the 'sim.matrix' argument to be able to perform cluster analysis.")
-    
-    cl <- amap::Kmeans(x        = genome.matrix[ , 2:ncol(genome.matrix)], 
-                       centers  = cl.centers,
-                       nstart   = cl.nstart, 
-                       iter.max = cl.iter.max, 
-                       method   = cl.method)
-    
-    colors <- bcolor(max(cl$cluster))
-    genome.matrix <- dplyr::mutate(genome.matrix, cl.colors = colors[cl$cluster])
-    
-    cor.value <- cor(genome.matrix$nLTRs,genome.matrix$genome.size, method = cor.method)
-    
-    colnames(genome.matrix)[1] <- "organism"
-    
-    if (!arrow_lab){
-      res <- ggplot2::ggplot(genome.matrix, ggplot2::aes(x = nLTRs, y = genome.size, colour = cl.colors)) + ggplot2::geom_point() + 
-        ggplot2::theme_minimal() + 
-        ggplot2::labs(x = xlab, y = ylab, title = paste0(main, " [ Corr ( ",cor.method," ) = ",round(cor.value, digits = 2), " ]")) + 
-        ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) + 
-        ggplot2::theme(axis.title  = ggplot2::element_text(size = text.size,face = "bold"),axis.text.y = ggplot2::element_text(size = text.size,face = "bold"),
-                       axis.text.x = ggplot2::element_text(size = text.size,face = "bold"),
-                       panel.background = ggplot2::element_blank(), 
-                       plot.title = ggplot2::element_text(size = text.size, colour = "black", face = "bold")) + ggplot2::geom_text(ggplot2::aes(label = organism), hjust = 0, vjust = 0, 
-                                                                                                                                   size = label.size, check_overlap = check.overlap)
-    }
-    
-    if (arrow_lab){
-      
-      res <- ggplot2::ggplot(genome.matrix, ggplot2::aes(x = nLTRs, y = genome.size, colour = cl.colors)) + ggplot2::geom_point() + 
-        ggplot2::theme_minimal() + 
-        ggplot2::labs(x = xlab, y = ylab, title = paste0(main, " [ Corr ( ",cor.method," ) = ",round(cor.value, digits = 2), " ]")) + 
-        ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) + 
-        ggplot2::theme(axis.title  = ggplot2::element_text(size = text.size,face = "bold"),axis.text.y = ggplot2::element_text(size = text.size,face = "bold"),
-                       axis.text.x = ggplot2::element_text(size = text.size,face = "bold"),
-                       panel.background = ggplot2::element_blank(), 
-                       plot.title = ggplot2::element_text(size = text.size, colour = "black", face = "bold")) + ggrepel::geom_text_repel(ggplot2::aes(label = organism), size = 2,fontface = 'bold')
-    }
-    
-    print(res)
-    return (cl)
-  } else {
-      
-    cor.value <- stats::cor(genome.matrix$nLTRs, genome.matrix$genome.size, method = cor.method)
-    
-    colnames(genome.matrix)[1] <- "organism"
-    
-    if (!arrow_lab){
-      
-      res <- ggplot2::ggplot(genome.matrix, ggplot2::aes(x = nLTRs, y = genome.size)) + ggplot2::geom_point() + 
-        ggplot2::theme_minimal() + 
-        ggplot2::labs(x = xlab, y = ylab, title = paste0(main, " [ Corr ( ",cor.method," ) = ",round(cor.value, digits = 2), " ]")) + 
-        ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) + 
-        ggplot2::theme(axis.title  = ggplot2::element_text(size = text.size,face = "bold"),axis.text.y = ggplot2::element_text(size = text.size,face = "bold"),
-                       axis.text.x = ggplot2::element_text(size = text.size,face = "bold"),
-                       panel.background = ggplot2::element_blank(), 
-                       plot.title = ggplot2::element_text(size = text.size, colour = "black", face = "bold")) + ggplot2::geom_text(ggplot2::aes(label = organism), hjust = 0, vjust = 0, 
-                                                                                                                                   size = label.size, check_overlap = check.overlap)
-    }
-    
-    if (arrow_lab){
-      res <- ggplot2::ggplot(genome.matrix, ggplot2::aes(x = nLTRs, y = genome.size)) + ggplot2::geom_point() + 
-        ggplot2::theme_minimal() + 
-        ggplot2::labs(x = xlab, y = ylab, title = paste0(main, " [ Corr ( ",cor.method," ) = ",round(cor.value, digits = 2), " ]")) + 
-        ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) + ggrepel::geom_text_repel(ggplot2::aes(label = organism), size = 2, fontface = 'bold')
-    }
-    
-    return (res)
-    
+    if (cl.analysis) {
+        if (is.null(sim.matrix))
+            stop ("Please specify the 'sim.matrix' argument to be able to perform cluster analysis.")
+        
+        if (!is.null(smooth.method)) {
+            if (!is.element(smooth.method, c("auto", "glm", "lm", "loess")))
+                stop (
+                    "Please specify a smooth.method that is supported by this function: 'auto', 'glm', 'lm', or 'loess'.",
+                    call. = FALSE
+                )
+        }
+        
+        cl <-
+            amap::Kmeans(
+                x        = genome.matrix[, 2:ncol(genome.matrix)],
+                centers  = cl.centers,
+                nstart   = cl.nstart,
+                iter.max = cl.iter.max,
+                method   = cl.method
+            )
+        
+        colors <- bcolor(max(cl$cluster))
+        genome.matrix <-
+            dplyr::mutate(genome.matrix, cl.colors = colors[cl$cluster])
+        
+        cor.value <-
+            cor(genome.matrix$nLTRs, genome.matrix$genome.size, method = cor.method)
+        
+        colnames(genome.matrix)[1] <- "organism"
+        
+        if (!arrow_lab) {
+            res <-
+                ggplot2::ggplot(genome.matrix,
+                                ggplot2::aes(
+                                    x = nLTRs,
+                                    y = genome.size,
+                                    colour = cl.colors
+                                )) + ggplot2::geom_point() +
+                ggplot2::theme_minimal() +
+                ggplot2::labs(
+                    x = xlab,
+                    y = ylab,
+                    title = paste0(
+                        main,
+                        " [ Corr ( ",
+                        cor.method,
+                        " ) = ",
+                        round(cor.value, digits = 2),
+                        " ]"
+                    )
+                ) +
+                ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) +
+                ggplot2::theme(
+                    axis.title  = ggplot2::element_text(size = text.size, face = "bold"),
+                    axis.text.y = ggplot2::element_text(size = text.size, face = "bold"),
+                    axis.text.x = ggplot2::element_text(size = text.size, face = "bold"),
+                    panel.background = ggplot2::element_blank(),
+                    plot.title = ggplot2::element_text(
+                        size = text.size,
+                        colour = "black",
+                        face = "bold"
+                    )
+                ) + ggplot2::geom_text(
+                    ggplot2::aes(label = organism),
+                    hjust = 0,
+                    vjust = 0,
+                    size = label.size,
+                    check_overlap = check.overlap
+                )
+            
+            if (!is.null(smooth.method)) {
+                res <- res + ggplot2::geom_smooth(method = smooth.method)
+            }
+        }
+        
+        if (arrow_lab) {
+            res <-
+                ggplot2::ggplot(genome.matrix,
+                                ggplot2::aes(
+                                    x = nLTRs,
+                                    y = genome.size,
+                                    colour = cl.colors
+                                )) + ggplot2::geom_point() +
+                ggplot2::theme_minimal() +
+                ggplot2::labs(
+                    x = xlab,
+                    y = ylab,
+                    title = paste0(
+                        main,
+                        " [ Corr ( ",
+                        cor.method,
+                        " ) = ",
+                        round(cor.value, digits = 2),
+                        " ]"
+                    )
+                ) +
+                ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) +
+                ggplot2::theme(
+                    axis.title  = ggplot2::element_text(size = text.size, face = "bold"),
+                    axis.text.y = ggplot2::element_text(size = text.size, face = "bold"),
+                    axis.text.x = ggplot2::element_text(size = text.size, face = "bold"),
+                    panel.background = ggplot2::element_blank(),
+                    plot.title = ggplot2::element_text(
+                        size = text.size,
+                        colour = "black",
+                        face = "bold"
+                    )
+                ) + ggrepel::geom_text_repel(ggplot2::aes(label = organism),
+                                             size = 3,
+                                             fontface = 'bold')
+        }
+        
+        if (!is.null(smooth.method)) {
+            res <- res + ggplot2::geom_smooth(method = smooth.method)
+        }
+        
+        print(res)
+        return (cl)
+    } else {
+        cor.value <-
+            stats::cor(genome.matrix$nLTRs, genome.matrix$genome.size, method = cor.method)
+        
+        colnames(genome.matrix)[1] <- "organism"
+        
+        if (!arrow_lab) {
+            res <-
+                ggplot2::ggplot(genome.matrix, ggplot2::aes(x = nLTRs, y = genome.size)) + ggplot2::geom_point() +
+                ggplot2::theme_minimal() +
+                ggplot2::labs(
+                    x = xlab,
+                    y = ylab,
+                    title = paste0(
+                        main,
+                        " [ Corr ( ",
+                        cor.method,
+                        " ) = ",
+                        round(cor.value, digits = 2),
+                        " ]"
+                    )
+                ) +
+                ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) +
+                ggplot2::theme(
+                    axis.title  = ggplot2::element_text(size = text.size, face = "bold"),
+                    axis.text.y = ggplot2::element_text(size = text.size, face = "bold"),
+                    axis.text.x = ggplot2::element_text(size = text.size, face = "bold"),
+                    panel.background = ggplot2::element_blank(),
+                    plot.title = ggplot2::element_text(
+                        size = text.size,
+                        colour = "black",
+                        face = "bold"
+                    )
+                ) + ggplot2::geom_text(
+                    ggplot2::aes(label = organism),
+                    hjust = 0,
+                    vjust = 0,
+                    size = label.size,
+                    check_overlap = check.overlap
+                )
+            
+            if (!is.null(smooth.method)) {
+                res <- res + ggplot2::geom_smooth(method = smooth.method)
+            }
+        }
+        
+        if (arrow_lab) {
+            res <-
+                ggplot2::ggplot(genome.matrix, ggplot2::aes(x = nLTRs, y = genome.size)) + ggplot2::geom_point() +
+                ggplot2::theme_minimal() +
+                ggplot2::labs(
+                    x = xlab,
+                    y = ylab,
+                    title = paste0(
+                        main,
+                        " [ Corr ( ",
+                        cor.method,
+                        " ) = ",
+                        round(cor.value, digits = 2),
+                        " ]"
+                    )
+                ) +
+                ggplot2::theme(legend.text = ggplot2::element_text(size = text.size)) +
+                ggplot2::theme(
+                    axis.title  = ggplot2::element_text(size = text.size, face = "bold"),
+                    axis.text.y = ggplot2::element_text(size = text.size, face = "bold"),
+                    axis.text.x = ggplot2::element_text(size = text.size, face = "bold"),
+                    panel.background = ggplot2::element_blank(),
+                    plot.title = ggplot2::element_text(
+                        size = text.size,
+                        colour = "black",
+                        face = "bold"
+                    )
+                ) + 
+                ggrepel::geom_text_repel(ggplot2::aes(label = organism),size = 3,fontface = 'bold')
+            
+            if (!is.null(smooth.method)) {
+                res <- res + ggplot2::geom_smooth(method = smooth.method)
+            }
+        }
+        
+        return (res)
+        
     }
 }
 
