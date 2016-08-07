d155195b007a715883e2a85acd38d5c54e8c8009
HajkD <hgd23@cam.ac.uk>
2016-08-07 12:21:14 +0100

    adding new function filter.uc()

---
+++ R/filter.uc.R
@@ -0,0 +1,27 @@
+#' @title Filter for cluster members
+#' @description Filter for cluster memebers in a \code{*.uc} format table.
+#' @param cluster.file a \code{data.frame} \code{*.uc} (USEARCH cluster) format (imported with \code{\link{read.uc}}).
+#' @author Hajk-Georg Drost
+#' @export
+
+filter.uc <- function(cluster.file){
+     
+     if (nrow(cluster.file) > 0) {
+         cluster.file.type.H <-
+             dplyr::filter(cluster.file, Type == "H")
+         cluster.file.type.H <-
+             dplyr::select(cluster.file.type.H,
+                           Cluster,
+                           Query,
+                           Target,
+                           Perc_Ident)
+         cluster.file.type.H <-
+             dplyr::mutate(cluster.file.type.H, Perc_Ident = as.numeric(Perc_Ident))
+         names(cluster.file.type.H) <-
+             paste0("Clust_", names(cluster.file.type.H))
+     } else {
+             stop("Your input cluster is empty.", call. = FALSE)
+     }
+     
+     return(cluster.file.type.H)
+ }
