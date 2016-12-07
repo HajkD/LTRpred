39a5b187e4e84a79c04ba236148ce21974294585
HajkD <hgd23@cam.ac.uk>
2016-12-07 17:06:39 +0000

    add global variables

--- R/pairwise.z.test.R
+++ R/pairwise.z.test.R
@@ -1,19 +1,22 @@
 
 pairwise.z.test <- function(sim.matrix){
   
-  sim.matrix <- as.data.frame(sim.matrix)
-  pvals <- vector("numeric",ncol(sim.matrix)-2)
-  for (i in 2:(ncol(sim.matrix)-1)){
-    pvals[i - 1] <- BSDA::z.test(x = sim.matrix[ , i],
-                                y = sim.matrix[ , i + 1],
-                                sigma.x = sd(sim.matrix[ , i]), 
-                                sigma.y = sd(sim.matrix[ , i + 1]),
-                                alternative = "two.sided")$p.value
+    sim.matrix <- as.data.frame(sim.matrix)
+    pvals <- vector("numeric", ncol(sim.matrix) - 2)
+    for (i in 2:(ncol(sim.matrix) - 1)) {
+        pvals[i - 1] <- BSDA::z.test(
+            x = sim.matrix[, i],
+            y = sim.matrix[, i + 1],
+            sigma.x = stats::sd(sim.matrix[, i]),
+            sigma.y = stats::sd(sim.matrix[, i + 1]),
+            alternative = "two.sided"
+        )$p.value
+        
+        names(pvals)[i - 1] <-
+            paste0(colnames(sim.matrix)[i], "<->", colnames(sim.matrix)[i + 1])
+    }
     
-    names(pvals)[i - 1] <- paste0(colnames(sim.matrix)[i],"<->",colnames(sim.matrix)[i + 1])
-  }
-  
-  return (pvals)
+    return (pvals)
   
 }
 
