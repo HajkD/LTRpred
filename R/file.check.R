f00f1bd79d6b59aed92f6599f9901363da5b99a6
HajkD <hgd23@cam.ac.uk>
2016-12-09 13:29:56 +0000

    add new internal function

---
+++ R/file.check.R
@@ -0,0 +1,10 @@
+file.check <- function(file.path) {
+    
+    if (!(file.access(file,path, 0) == 0)) {
+        stop("File '",file.path,"' does not exist.", call. = FALSE)
+    }
+    
+    if (!(file.access(file,path, 4) == 0)) {
+        stop("File '",file.path,"' does not have read permission.", call. = FALSE)
+    }
+}
