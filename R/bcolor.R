316724e0debcaa0e42193e39bc719bf266b42008
HajkD <hajkdrost@googlemail.com>
2017-01-12 11:51:58 +0100

    remove analysis and vis funcs and move to new package LTRlearn

--- R/bcolor.R
+++
@@ -1,9 +0,0 @@
-#' @title Beautiful colors for plots
-#' @description A distinguished color scheme for scientific plots.
-#' @param n number of different colors that shall be returned.
-#' @author Hajk-Georg Drost
-#' @examples
-#' # return 5 colors 
-#' bcolor(5)
-#' @export
-bcolor <- function(n) return(grDevices::colorRampPalette(RColorBrewer::brewer.pal(8,"Dark2"))(n))
