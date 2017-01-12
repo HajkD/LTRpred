0b09fdfc7224f65323b8b92719a7d1ba2dcdfcca
HajkD <hajkdrost@googlemail.com>
2017-01-12 11:52:17 +0100

    fix linking

--- R/cn2bed.R
+++ R/cn2bed.R
@@ -1,7 +1,7 @@
 #' @title Write copy number estimation results to BED file format.
 #' @description Format copy number estimation output to BED file format.
-#' @param cn.pred \code{data.frame} object returned by \code{\link{ltr.cn}} (if \code{type = "solo"}) or \code{\link{te.cn}} (if \code{type = "te"}).
-#' @param type type of copy number estimation: \code{\link{ltr.cn}} (if \code{type = "solo"}) or \code{\link{te.cn}} (if \code{type = "te"}).
+#' @param cn.pred \code{data.frame} object returned by \code{\link{ltr.cn}} (if \code{type = "solo"}).
+#' @param type type of copy number estimation: \code{\link{ltr.cn}} (if \code{type = "solo"}).
 #' @param filename name of the output file (will be extended by "*.csv").
 #' @param sep column separator.
 #' @param output path in which the output file shall be stored.
@@ -9,7 +9,11 @@
 #' @seealso \code{\link{ltr.cn}}, \code{\link{LTRpred}}
 #' @export
 
-cn2bed <- function(cn.pred, type = "solo", filename = "copy_number_est", sep = "\t", output = NULL) {
+cn2bed <- function(cn.pred, 
+                   type = "solo", 
+                   filename = "copy_number_est", 
+                   sep = "\t", 
+                   output = NULL) {
     
     if (!is.element(type, c("solo", "te")))
         stop("Please choose either 'solo' or 'te' as type.")
