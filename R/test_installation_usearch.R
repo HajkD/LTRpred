bb55c9b1e2786b77a1fbfad30438b5d65e1b6e88
HajkD <hgd23@cam.ac.uk>
2017-04-25 11:37:40 +0100

    add local installation test

---
+++ R/test_installation_usearch.R
@@ -0,0 +1,20 @@
+#' @title Helper function to test USEARCH installation
+#' @noRd
+test_installation_usearch <- function() {
+    # test if a valid USEARCH version is installed
+    tryCatch({
+        sys_out <-
+            system("usearch --version", intern = TRUE)
+    }, error = function(e)
+        stop(
+            "It seems like you don't have USEARCH installed locally on your machine
+            or the PATH variable to the USEARCH program is not set correctly.",
+            "Please consult the Installation vignette or https://github.com/genometools/genometools 
+            for details on how to install USEARCH.",
+            call. = FALSE
+        ))
+    
+    if (any(stringr::str_detect(sys_out, "usearch")))
+        return(TRUE)
+    
+}
