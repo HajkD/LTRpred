28c037e6f488c9386892d776e78d8cec9253c633
HajkD <hgd23@cam.ac.uk>
2017-04-25 11:37:17 +0100

    add local installation test and message update

--- R/ORFpred.R
+++ R/ORFpred.R
@@ -19,54 +19,100 @@ ORFpred <- function(seq.file,
                     min.codons = 200, 
                     trans.seqs = FALSE,
                     output     = NULL){
-  
-  
-  if (!trans.seqs){
+   
+    test_installation_usearch()
     
-    if (is.null(output)){
-      system(paste0("usearch -fastx_findorfs ",ws.wrap.path(seq.file),
-                    " -ntout ",paste0(basename(seq.file),"_nt.fsa"),
-                    " -orfstyle ",orf.style," -mincodons ",min.codons))
+    if (!trans.seqs) {
+        if (is.null(output)) {
+            system(
+                paste0(
+                    "usearch -fastx_findorfs ",
+                    ws.wrap.path(seq.file),
+                    " -ntout ",
+                    paste0(basename(seq.file), "_nt.fsa"),
+                    " -orfstyle ",
+                    orf.style,
+                    " -mincodons ",
+                    min.codons
+                )
+            )
+        } else {
+            system(
+                paste0(
+                    "usearch -fastx_findorfs ",
+                    ws.wrap.path(seq.file),
+                    " -ntout ",
+                    file.path(output, paste0(basename(seq.file), "_nt.fsa")),
+                    " -orfstyle ",
+                    orf.style,
+                    " -mincodons ",
+                    min.codons
+                )
+            )
+        }
+        
     } else {
-      
-      system(paste0("usearch -fastx_findorfs ",ws.wrap.path(seq.file),
-                    " -ntout ",file.path(output,paste0(basename(seq.file),"_nt.fsa")),
-                    " -orfstyle ",orf.style," -mincodons ",min.codons))
+        if (is.null(output)) {
+            system(
+                paste0(
+                    "usearch -fastx_findorfs ",
+                    ws.wrap.path(seq.file),
+                    " -ntout ",
+                    paste0(basename(seq.file), "_nt.fsa"),
+                    " -aaout ",
+                    paste0(basename(seq.file), "_aa.fsa"),
+                    " -orfstyle ",
+                    orf.style,
+                    " -mincodons ",
+                    min.codons
+                )
+            )
+        } else {
+            system(
+                paste0(
+                    "usearch -fastx_findorfs ",
+                    ws.wrap.path(seq.file),
+                    " -ntout ",
+                    file.path(output, paste0(
+                        basename(seq.file), "_orfs_nt.fsa"
+                    )),
+                    " -aaout ",
+                    file.path(output, paste0(
+                        basename(seq.file), "_orfs_aa.fsa"
+                    )),
+                    " -orfstyle ",
+                    orf.style,
+                    " -mincodons ",
+                    min.codons
+                )
+            )
+        }
     }
     
-  } else {
+    if (!is.null(output)) {
+        orf.file <- file.path(output, paste0(basename(seq.file), "_nt.fsa"))
+        if (file.info(orf.file)$size == 0 ||
+            is.na(file.info(orf.file)$size == 0)) {
+            ReadSeqFile <- Biostrings::readDNAStringSet(seq.file)
+            ORFCount.df <-
+                dplyr::data_frame(seq.id = ReadSeqFile@ranges@NAMES,
+                                  orfs = rep(0, length(ReadSeqFile@ranges@NAMES)))
+        } else {
+            ORFCount.df <- read.orfs(orf.file)
+        }
+    } else {
+        orf.file <- paste0(basename(seq.file), "_nt.fsa")
+        if (file.info(orf.file)$size == 0 ||
+            is.na(file.info(orf.file)$size == 0)) {
+            ReadSeqFile <- Biostrings::readDNAStringSet(seq.file)
+            ORFCount.df <-
+                dplyr::data_frame(seq.id = ReadSeqFile@ranges@NAMES,
+                                  orfs = rep(0, length(ReadSeqFile@ranges@NAMES)))
+        } else {
+            ORFCount.df <- read.orfs(orf.file)
+        }
+    }
     
-    if (is.null(output)){
-      system(paste0("usearch -fastx_findorfs ",ws.wrap.path(seq.file),
-                    " -ntout ",paste0(basename(seq.file),"_nt.fsa"),
-                    " -aaout ",paste0(basename(seq.file),"_aa.fsa"),
-                    " -orfstyle ",orf.style," -mincodons ",min.codons))
-    } else {
-      system(paste0("usearch -fastx_findorfs ",ws.wrap.path(seq.file),
-                    " -ntout ",file.path(output,paste0(basename(seq.file),"_orfs_nt.fsa")),
-                    " -aaout ",file.path(output,paste0(basename(seq.file),"_orfs_aa.fsa")),
-                    " -orfstyle ",orf.style," -mincodons ",min.codons))
-    }
-  }
-  
-  if (!is.null(output)){
-    orf.file <- file.path(output,paste0(basename(seq.file),"_nt.fsa"))
-    if (file.info(orf.file)$size == 0 || is.na(file.info(orf.file)$size == 0)){
-      ReadSeqFile <- Biostrings::readDNAStringSet(seq.file)
-      ORFCount.df <- dplyr::data_frame(seq.id = ReadSeqFile@ranges@NAMES, orfs = rep(0,length(ReadSeqFile@ranges@NAMES)))
-    } else {
-      ORFCount.df <- read.orfs(orf.file)
-    }
-  } else {
-    orf.file <- paste0(basename(seq.file),"_nt.fsa")
-    if (file.info(orf.file)$size == 0 || is.na(file.info(orf.file)$size == 0)){
-      ReadSeqFile <- Biostrings::readDNAStringSet(seq.file)
-      ORFCount.df <- dplyr::data_frame(seq.id = ReadSeqFile@ranges@NAMES, orfs = rep(0,length(ReadSeqFile@ranges@NAMES)))
-    } else {
-    ORFCount.df <- read.orfs(orf.file)
-    }
-  }
-  
-  return (ORFCount.df)
+    return(ORFCount.df)
 }
 
