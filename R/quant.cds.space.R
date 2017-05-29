15842a161a478916fb0313bac3b397b7fc8705fa
HajkD <hgd23@cam.ac.uk>
2017-05-29 12:48:22 +0100

    add new func

---
+++ R/quant.cds.space.R
@@ -0,0 +1,17 @@
+#' @title Quantify the coding sequence space within a genome
+#' @description Quantification of the cds space (= total length of
+#' all annotated coding sequences) within a given genome of interest.
+#' @param file file path to a fasta file storing the cds sequences.
+#' @author Hajk-Georg Drost
+#' @seealso \code{\link{quant.protein.space}}, \code{\link{quant.repeat.space}},
+#' \code{\link{quant.meta.space}}
+#' @export
+quant.cds.space <- function(file) {
+    cds_seqs <- biomartr::read_cds(file = file)
+    cds_space_in_Mbp <- sum(cds_seqs@ranges@width) / 1000000L
+    nCDSs <- length(cds_seqs)
+    res <-
+        tibble::tibble(CDSSpaceMbp = cds_space_in_Mbp, nCDSs = nCDSs)
+    return(res)
+}
+
