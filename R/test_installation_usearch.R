7f58c7890f06d3a63ab42755f1e3f032e681075f
HajkD <hgd23@cam.ac.uk>
2017-10-31 18:04:47 +0000

    update error message

--- R/test_installation_usearch.R
+++ R/test_installation_usearch.R
@@ -7,10 +7,8 @@ test_installation_usearch <- function() {
             system("usearch --version", intern = TRUE)
     }, error = function(e)
         stop(
-            "It seems like you don't have USEARCH installed locally on your machine
-            or the PATH variable to the USEARCH program is not set correctly.",
-            "Please consult the Installation vignette or https://github.com/genometools/genometools 
-            for details on how to install USEARCH.",
+            "It seems like you don't have USEARCH installed locally on your machine or the PATH variable to the USEARCH program is not set correctly. ",
+            "Please consult the Installation vignette or http://drive5.com/usearch/download.html for details on how to install USEARCH.",
             call. = FALSE
         ))
     
