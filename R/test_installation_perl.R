1a022cf4f6156e47571082bd6a1e5923bac1313b
HajkD <hajkdrost@googlemail.com>
2017-11-10 14:48:37 +0000

    update error message

--- R/test_installation_perl.R
+++ R/test_installation_perl.R
@@ -7,10 +7,8 @@ test_installation_perl <- function() {
             system("perl -v", intern = TRUE)
     }, error = function(e)
         stop(
-            "It seems like you don't have Perl installed locally on your machine
-            or the PATH variable to Perl is not set correctly.",
-            "Please consult the Installation vignette or https://www.perl.org/ 
-            for details on how to install Perl on your system.",
+            "It seems like you don't have Perl installed locally on your machine or the PATH variable to Perl is not set correctly.",
+            "Please consult the Installation vignette or https://www.perl.org/ for details on how to install Perl on your system.",
             call. = FALSE
         ))
     
