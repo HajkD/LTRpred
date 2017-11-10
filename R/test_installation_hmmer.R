7e79cb424ac58a4f7942a84ea3ac478789914fc2
HajkD <hajkdrost@googlemail.com>
2017-11-10 14:48:32 +0000

    update error message

--- R/test_installation_hmmer.R
+++ R/test_installation_hmmer.R
@@ -7,10 +7,8 @@ test_installation_hmmer <- function() {
             system("hmmpress -h", intern = TRUE)
     }, error = function(e)
         stop(
-            "It seems like you don't have HMMer installed locally on your machine
-            or the PATH variable to the HMMer program is not set correctly.",
-            "Please consult the Installation vignette or http://hmmer.org/download.html
-            for details on how to install HMMer",
+            "It seems like you don't have HMMer installed locally on your machine or the PATH variable to the HMMer program is not set correctly.",
+            "Please consult the Installation vignette or http://hmmer.org/download.html for details on how to install HMMer",
             call. = FALSE
         ))
     
