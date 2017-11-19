f8f0defb4e9decc88a26e3320e2ee7716113cd25
HajkD <hajkdrost@googlemail.com>
2017-11-19 18:21:58 +0000

    update dfam messages

--- R/dfam.query.R
+++ R/dfam.query.R
@@ -37,22 +37,27 @@ dfam.query <- function(seq.file,
         stop("The file '",seq.file,"' does not exist! Please make sure that a correct path to the sequence file is passed to the dfam.query() function.", call. = FALSE)
     
     if (Dfam.db == "download") {
+      message("Download Dfam database from http://dfam.org/web_download/Current_Release/Dfam.hmm.gz ...")
         downloader::download(
             "http://dfam.org/web_download/Current_Release/Dfam.hmm.gz",
             file.path(output.folder, "Dfam.hmm.gz"),
             mode = "wb"
         )
-        
+        message("Download completed!")
         message("Prepare the Dfam.hmm database...")
         
-        system(paste0("hmmpress ", file.path(
+        hmmpress <- system(paste0("hmmpress ", file.path(
             ws.wrap.path(output.folder), gzfile("Dfam.hmm.gz")
-        )))
+        )), intern = TRUE)
+        
+        if (attr(hmmpress, "status") > 0)
+          stop("hmmpress could not format the file ",file.path(
+            ws.wrap.path(output.folder), gzfile("Dfam.hmm.gz")), ". Is hmmpress installed on your system and did the download process of the Dfam database work properly? ", call. = FALSE)
         
         message("Run Dfam scan...")
         # make sure that in future versions the PATH variable is set and OSX, Linux,
         # and Windows paths will be supported
-        system(
+        dfamscan <- system(
             paste0(
                 "perl ",
                 "/usr/local/bin/dfamscan.pl",
@@ -73,16 +78,19 @@ dfam.query <- function(seq.file,
                     output.folder, paste0(basename(seq.file), "_logfile.txt")
                 )) ,
                 " --masking_thresh "
-            )
+            ), intern = TRUE
         )
+        
+        if (attr(dfamscan, "status") > 0)
+          stop("dfamscan could not be run properly! Did you store the file 'dfamscan.pl' in '/usr/local/bin/dfamscan.pl' ?", call. = FALSE)
         
         message("Finished Dfam scan!")
         message("A dfam query file has been generated and stored at",ws.wrap.path(file.path(
             output.folder, paste0(basename(seq.file), "_DfamAnnotation.out"))),".")
     } else {
-        if (file.exists(file.path(ws.wrap.path(output.folder), "Dfam.hmm.h3f")) &&
-            file.exists(file.path(ws.wrap.path(output.folder), "Dfam.hmm.h3i")) &&
-            file.exists(file.path(ws.wrap.path(output.folder), "Dfam.hmm.h3m")) &&
+        if (file.exists(file.path(ws.wrap.path(output.folder), "Dfam.hmm.h3f")) &
+            file.exists(file.path(ws.wrap.path(output.folder), "Dfam.hmm.h3i")) &
+            file.exists(file.path(ws.wrap.path(output.folder), "Dfam.hmm.h3m")) &
             file.exists(file.path(ws.wrap.path(output.folder), "Dfam.hmm.h3p"))) {
             message("Prepare the Dfam.hmm database...")
             system(paste0("hmmpress ", file.path(Dfam.db, "Dfam.hmm")))
