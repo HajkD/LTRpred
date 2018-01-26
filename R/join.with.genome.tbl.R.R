fb1d2a8c270f8402447ec1c02b6989c17562d555
HajkD <hajkdrost@googlemail.com>
2018-01-26 14:39:03 +0000

    add new function join.with.genome.tbl()

---
+++ R/join.with.genome.tbl.R.R
@@ -0,0 +1,17 @@
+#' @title Join \code{gm_files} returned by \code{generate.multi.quality.filter.meta} with a genome information table
+#' @description  
+#' @param meta.summary.file
+#' @param genome.tbl
+#' @author Hajk-Georg Drost
+#' @export
+
+join.with.genome.tbl <- function(meta.summary.file, genome.tbl) {
+  
+  for (i in seq_len(length(meta.summary.file))) {
+    meta.summary.file[[i]]$gm_file <-
+      dplyr::left_join(meta.summary.file[[i]]$gm_file, genome.tbl, by = "organism")
+    meta.summary.file[[i]]$gm_file <- dplyr::filter(meta.summary.file[[i]]$gm_file, !is.na(order))
+  }
+  
+  return(meta.summary.file)
+}
