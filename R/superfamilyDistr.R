45324dc0ac4de787111fda0ceffce93aaec8d475
HajkD <hajkdrost@googlemail.com>
2020-01-23 19:29:18 +0100

    add global variable binding

--- R/superfamilyDistr.R
+++ R/superfamilyDistr.R
@@ -1,5 +1,8 @@
 superfamilyDistr <- function(blast.file) {
   
+  query_id <- subject_id <- evalue <- species <- superfamily <- NULL
+  n_copia <- n_gypsy <- n_total <- repbase_blast_summarized_test_summary <- NULL
+  
   repbase_blast <- readr::read_csv(blast.file, col_names = TRUE)
   
   repbase_blast_summarized <- dplyr::summarize(dplyr::group_by(repbase_blast, query_id, subject_id), evalue = min(evalue))
@@ -16,12 +19,12 @@ superfamilyDistr <- function(blast.file) {
       n_gypsy = sum(stringr::str_detect(superfamily, "Gypsy")),
       prop_copia = n_copia / (n_copia + n_gypsy),
       prop_gypsy = n_gypsy / (n_copia + n_gypsy),
-      n_total = n(),
+      n_total = dplyr::n(),
       n_other = n_total - (n_copia + n_gypsy)
     )
   
   
-  readr::write_excel_csv(repbase_blast_summarized_summary, paste0("repbase_blast_summarized_summary_",basename(blast.file) ,".csv"))
+  readr::write_excel_csv(repbase_blast_summarized_summary, paste0("repbase_blast_summarized_summary_", basename(blast.file) ,".csv"))
   
   return(repbase_blast_summarized_test_summary)
 }
