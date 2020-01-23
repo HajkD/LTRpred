d618bbb5c55a154f9efd530e4e638c5c45c14dac
HajkD <hajkdrost@googlemail.com>
2020-01-23 19:29:07 +0100

    update docu

--- R/quant.protein.space.R
+++ R/quant.protein.space.R
@@ -3,8 +3,6 @@
 #' all annotated protein coding genes) within a given genome of interest.
 #' @param file file path to a fasta file storing the protein sequences.
 #' @author Hajk-Georg Drost
-#' @seealso \code{\link{quant.cds.space}}, \code{\link{quant.repeat.space}},
-#' \code{\link{quant.meta.space}}
 #' @export
 quant.protein.space <- function(file) {
     prot_seqs <- biomartr::read_proteome(file = file)
