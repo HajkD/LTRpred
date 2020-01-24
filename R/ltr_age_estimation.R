520e8c25b2d6a56a07280f6e06b657e3a9bf1712
HajkD <hajkdrost@googlemail.com>
2020-01-24 16:21:43 +0100

    add new functionality to LTRpred: molecular evolutionary dating of TE insertions

--- R/ltr_age_estimation.R
+++ R/ltr_age_estimation.R
@@ -1,6 +1,7 @@
 #' @title Estimate retrotransposon insertion age in Mya based on 5 prime and 3 prime LTR sequence homology
 #' @description This function implements diverse metrics to roughly estimate
 #' the insertion age in Mya based on 5 prime and 3 prime LTR sequence homology.
+#' @param pred a prediction file generated with \code{\link{LTRpred}}.
 #' @param ltr_seqs_3_prime file path to a fasta file storing the sequences of the respective 3 prime LTR (e.g. as annotatted by \code{\link{LTRpred}}).
 #' @param ltr_seqs_5_prime file path to a fasta file storing the sequences of the respective 5 prime LTR (e.g. as annotatted by \code{\link{LTRpred}}).
 #' @param model a model as specified in \code{\link[ape]{dist.dna}}: a character string specifying the evolutionary model to be used - must be one of
@@ -24,23 +25,27 @@
 #' @param mutation_rate a mutation rate per site per year. For retrotransposons the default is \eqn{mutation_rate = 1.3 * 10E-8} (Wicker and Keller, 2007).
 #' @author Hajk-Georg Drost
 #' @examples \dontrun{
+#' ltr_pred <- LTRpred::read.ltrpred(
+#'           system.file("Hsapiens_ChrY_LTRpred_DataSheet.tsv", 
+#'           package = "LTRpred"))
 #' # define file path to fasta file storing 3 prime LTR sequences
 #' ltr_seqs_3_prime <- system.file("Hsapiens_ChrY-ltrdigest_3ltr.fas", package = "LTRpred")
 #' ltr_seqs_5_prime <- system.file("Hsapiens_ChrY-ltrdigest_5ltr.fas", package = "LTRpred")
 #' # estimate insertion age based on 3 prime and 5 prime LTR homology using the K80 model
-#' Hsapiens_ltr_age <- LTRpred::ltr_age_estimation(ltr_seqs_3_prime, ltr_seqs_5_prime)
+#' Hsapiens_ltr_age <- LTRpred::ltr_age_estimation(ltr_pred, ltr_seqs_3_prime, ltr_seqs_5_prime)
 #' # look at results
 #' Hsapiens_ltr_age
 #' }
 #' @export
 
 ltr_age_estimation <-
-  function(ltr_seqs_3_prime,
+  function(pred,
+           ltr_seqs_3_prime,
            ltr_seqs_5_prime,
            model = "K80",
            mutation_rate = 1.3 * 10E-8) {
     
-    message("Starting retrotransposon evolutionary age estimation by comparing the 3' and 5' LTRs using the molecular evolution model '", model, "' and the mutation rate '", mutation_rate, "' (please make sure the mutation rate can be assumed for your species of interest!) ...")
+    message("Starting retrotransposon evolutionary age estimation by comparing the 3' and 5' LTRs using the molecular evolution model '", model, "' and the mutation rate '", mutation_rate, "' (please make sure the mutation rate can be assumed for your species of interest!) for ", nrow(pred), " predicted elements ...")
     message("\n")
     message("Please be aware that evolutionary age estimation based on 3' and 5' LTR comparisons are only very rough time estimates and don't take reverse-transcription mediated retrotransposon recombination between family members of retroelements into account! Please consult Sanchez et al., 2017 Nature Communications and Drost & Sanchez, 2019 Genome Biology and Evolution for more details on retrotransposon recombination.")
     
@@ -50,9 +55,14 @@ ltr_age_estimation <-
     if (!file.exists(ltr_seqs_5_prime))
       stop("The file '", ltr_seqs_5_prime, "' does not seem to exist. Please provide a valid file path for argument 'ltr_seqs_5_prime'.", call. = FALSE)
     
+    new_ltr_seqs_3_prime <- file.path(tempdir(), basename(ltr_seqs_3_prime))
+    new_ltr_seqs_5_prime <- file.path(tempdir(), basename(ltr_seqs_5_prime))
+    pred2fasta(pred, ltr_seqs_3_prime, new_ltr_seqs_3_prime)
+    pred2fasta(pred, ltr_seqs_5_prime, new_ltr_seqs_5_prime)
+    
     # import 3' and 5' LTR sequences
-    ltr_3_prime_seqs <- Biostrings::readDNAStringSet(ltr_seqs_3_prime)
-    ltr_5_prime_seqs <- Biostrings::readDNAStringSet(ltr_seqs_5_prime)
+    ltr_3_prime_seqs <- Biostrings::readDNAStringSet(new_ltr_seqs_3_prime)
+    ltr_5_prime_seqs <- Biostrings::readDNAStringSet(new_ltr_seqs_5_prime)
     
     if (length(ltr_3_prime_seqs) != length(ltr_5_prime_seqs))
       stop("Something went wrong! The files '",ltr_seqs_3_prime,"' and '",ltr_seqs_5_prime,"' don't contain the same number of LTR sequences.", call. = FALSE)
@@ -122,6 +132,7 @@ ltr_age_estimation <-
     }
     
     res <- tibble::tibble(
+      orf.id = pred$orf.id,
       ltr_name = names(ltr_3_prime_seqs),
       ltr_age_mya = (dist_vals / (2 * mutation_rate)) / 1E6,
       ltr_aln_score = Alignments@score,
