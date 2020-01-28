4b237278efd390cb4008e6eaf568963e115a31d1
HajkD <hajkdrost@googlemail.com>
2020-01-28 12:40:36 +0100

    add new arguments

--- R/plot_element_distr_along_chromosome.R
+++ R/plot_element_distr_along_chromosome.R
@@ -3,7 +3,8 @@
 #' will be visualized along the chromsome.
 #' @param pred LTRpred.tbl generated with \code{\link{LTRpred}}.
 #' @param genome.file a file path to the genome assembly file in fasta format for which chromosomes shall be visualized.
-#' @param centromere_starts a numeric vector containing the start coordinates of the centromers (for all chromosomes in \code{genome.file}).
+#' @param centromere_start a numeric vector storing the centromere start coordinates in the \code{genome.file}. The position in the numeric vector should correspond to the chromosome name in the \code{genome.file} fasta file. If \code{centromere_start = NULL} (default), then no centromeres will be drawn.
+#'@param centromere_end a numeric vector storing the centromere end coordinates in the \code{genome.file}. The position in the numeric vector should correspond to the chromosome name in the \code{genome.file} fasta file. If \code{centromere_end = NULL} (default), then no centromeres will be drawn.
 #' @param ... additional arguments that shall be passed to the visualization function \code{\link[ggbio]{autoplot}}.
 #' @author Hajk-Georg Drost
 #' @examples \dontrun{
@@ -17,20 +18,35 @@
 #' }
 #' @export
 
-plot_element_distr_along_chromosome <- function(pred, genome.file, centromere_starts, ...){
-  
-  if (!file.exists(genome.file))
+plot_element_distr_along_chromosome <-
+  function(pred,
+           genome.file,
+           centromere_start = NULL,
+           centromere_end = NULL,
+           ...) {
+    if (!file.exists(genome.file))
     stop("The genome.file '", genome.file, "' does not seem to exist. Please provide a valid path to a genome file in fasta format.", call. = FALSE)
   
-  if (!is.numeric(centromere_starts))
-    stop("Please provide numeric values for centromere starts.", call. = FALSE)
-  
-  genome <- Biostrings::readDNAStringSet(genome.file)
-  
-  if (length(genome) != length(centromere_starts))
-    stop("The number of chroosomes (= ", length(genome), ") stored in your genome file doesn't match the number of centromere start coordinates (= ",length(centromere_starts), ")." , call. = FALSE)
-  
-
+  if (!is.null(centromere_start) & !is.null(centromere_end)) {
+    if (!is.numeric(centromere_start))
+      stop("Please provide numeric values for centromere starts.", call. = FALSE)
+    if (!is.numeric(centromere_end))
+      stop("Please provide numeric values for centromere end", call. = FALSE)
+    
+    if (length(centromere_start) != length(centromere_end))
+      stop("Your centromere_start and centromere_end vectors have different lengthsm please make sure that the start and end coordinates match.", call. = FALSE)
+    
+    genome <- Biostrings::readDNAStringSet(genome.file)
+    
+    if (length(genome) != length(centromere_start))
+      stop("The number of chroosomes (= ", length(genome), ") stored in your genome file doesn't match the number of centromere start coordinates (= ",length(centromere_start), ")." , call. = FALSE)
+    
+  }
+    
+  if (is.null(centromere_start) & is.null(centromere_end)) {
+      genome <- Biostrings::readDNAStringSet(genome.file)
+  }
+    
   if (!all(genome@ranges@NAMES == unique(pred$chromosome)))
     stop("The chromosome names from the genome.file (", paste0(genome@ranges@NAMES , collapse = ", "), ") and the chromosome names from the pred file (",paste0(unique(pred$chromosome), collapse = ", "), ") do not match. Please fix this for this function to work.", call. = FALSE)
   
@@ -38,7 +54,9 @@ plot_element_distr_along_chromosome <- function(pred, genome.file, centromere_st
   GenomeInfoDb::seqlengths(dn) <- genome@ranges@width
   dn <- GenomeInfoDb::keepSeqlevels(dn, genome@ranges@NAMES)
   
-  ggbio::autoplot(dn, layout = "karyogram", ...)
+  p <- ggbio::autoplot(dn, layout = "karyogram", ...)
+  
+  return(p)
   
   #ggbio::plotKaryogram(dn, ...) 
   
